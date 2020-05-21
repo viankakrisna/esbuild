9f8f2f3e298622cde705f702413d354734168a2e
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2020-05-21 18:56:43 +0700

    fallback to the parent of output file

--- internal/bundler/bundler.go
+++ internal/bundler/bundler.go
@@ -164,7 +164,11 @@ func parseFile(
 
 	case LoaderURL:
 		url := path.Base(absolutePath)
-		ioutil.WriteFile(path.Join(bundleOptions.AbsOutputDir, url), []byte(source.Contents), 0644)
+		targetFolder := bundleOptions.AbsOutputDir
+		if targetFolder == "" {
+			targetFolder = path.Dir(bundleOptions.AbsOutputFile)
+		}
+		defer ioutil.WriteFile(path.Join(targetFolder, url), []byte(source.Contents), 0644)
 		expr := ast.Expr{ast.Loc{0}, &ast.EString{lexer.StringToUTF16(url)}}
 		ast := parser.ModuleExportsAST(log, source, parseOptions, expr)
 		results <- parseResult{source, ast, true}
